1601b4591de7f39527e9e53b6f8f63202b2f4546
TappChat-dev <tappchat.dev@gmail.com>
2021-08-05 12:46:24 +0530

    add network manager and change in login controller

--- SMAC/SMAC/Login/View/LoginViewController.swift
+++ SMAC/SMAC/Login/View/LoginViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginView: UIView!
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var btnLogin: UIButton!
     @IBOutlet weak var btnHideUnhide: UIButton!
     @IBOutlet weak var btnForgetPassword: UIButton!
-
+    @IBOutlet weak var btnSegment:UISegmentedControl!
+    var segmentSelectedOption:String?
+    lazy var viewModel = {
+        LoginViewModel()
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        usernameTxt.delegate = self
+        passwordTxt.delegate = self
         // Do any additional setup after loading the view.
     }
     
@@ -37,8 +42,52 @@ class LoginViewController: UIViewController {
     @IBAction func tapToLogin(_ sender:UIButton){
 
     }
+    
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        switch btnSegment.selectedSegmentIndex {
+        case 0:
+            segmentSelectedOption = "icg"
+            break
+            
+        case 1:
+            segmentSelectedOption = "non-icg"
+            break
+           
+        default:
+            break;
+        }
+    }
 }
 
 extension LoginViewController: UITextFieldDelegate{
-    
+    // UITextField Delegates
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            print("TextField did begin editing method called")
+        }
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            print("TextField did end editing method called\(textField.text!)")
+        }
+        func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+            print("TextField should begin editing method called")
+            return true;
+        }
+        func textFieldShouldClear(_ textField: UITextField) -> Bool {
+            print("TextField should clear method called")
+            return true;
+        }
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            print("TextField should end editing method called")
+            return true;
+        }
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            print("While entering the characters this method gets called")
+            return true;
+        }
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            print("TextField should return method called")
+            textField.resignFirstResponder();
+            return true;
+        }
 }
+
+
