82dea2ee3864c5ef3a6d7e9862783215c4e12dec
TappChat-dev <tappchat.dev@gmail.com>
2021-08-18 16:35:31 +0530

    add picker on update tickets

--- SMAC/SMAC/UpdateTickets/View/UpdateTicketViewController.swift
+++ SMAC/SMAC/UpdateTickets/View/UpdateTicketViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
+import MobileCoreServices
+import UniformTypeIdentifiers
+import Foundation
 
-class UpdateTicketViewController: UIViewController {
+class UpdateTicketViewController: UIViewController,UINavigationControllerDelegate {
     @IBOutlet weak var serviceRequesterTxt: UITextField!
     @IBOutlet weak var titleTxt: UITextField!
         @IBOutlet weak var contractNameTxt: UITextField!
@@ -53,6 +57,7 @@ class UpdateTicketViewController: UIViewController {
         self.navigationController?.popViewController( animated: true)
     }
     
+    // MARK: - Date Picker Button
     @IBAction func tapToDatePicker(_ sender:Any){
         datePicker = UIDatePicker.init()
             datePicker.backgroundColor = UIColor.white
@@ -92,4 +97,158 @@ class UpdateTicketViewController: UIViewController {
         toolbar.removeFromSuperview()
         datePicker.removeFromSuperview()
     }
+    
+    // MARK: - Action Sheet Picker
+    func actionSheetOption(){
+        let actionSheet = UIAlertController(title: "Choose Option", message: nil, preferredStyle: .actionSheet)
+        let actionSelectCamera = UIAlertAction(title: "Camera", style: .default, handler: {
+            UIAlertAction in
+            self.openCamera()
+        })
+        let actionSelectGallery = UIAlertAction(title: "Gallery", style: .default, handler: {
+            UIAlertAction in
+            self.openGallary()
+        })
+        let actionSelectDocument = UIAlertAction(title: "Document", style: .default, handler: {
+            UIAlertAction in
+            self.clickDocumentFunction()
+        })
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        actionSheet.addAction(actionCancel)
+        actionSheet.addAction(actionSelectCamera)
+        actionSheet.addAction(actionSelectGallery)
+        actionSheet.addAction(actionSelectDocument)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Camera
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }else{
+            Utility().addAlertView("Warning!", "You don't have camera", "OK", self)
+        }
+    }
+    // MARK: - Gallary
+    func openGallary(){
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.delegate = self
+        imagePicker.modalPresentationStyle = .overFullScreen
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - Document
+    func clickDocumentFunction(){
+
+        let types: [String] = [
+            kUTTypeJPEG as String,
+            kUTTypePNG as String,
+            "com.microsoft.word.doc",
+            "org.openxmlformats.wordprocessingml.document",
+            kUTTypeRTF as String,
+            "com.microsoft.powerpoint.​ppt",
+            "org.openxmlformats.presentationml.presentation",
+            kUTTypePlainText as String,
+            "com.microsoft.excel.xls",
+            "org.openxmlformats.spreadsheetml.sheet",
+            "com.apple.iwork.pages.pages",
+            "com.apple.iwork.numbers.numbers",
+            "com.apple.iwork.keynote.key",
+            "public.image",
+            "public.text",
+            "public.zip-archive",
+            "com.pkware.zip-archive",
+            "public.data",
+            kUTTypePDF as String,
+            kUTTypeMP3 as String,
+            kUTTypeSpreadsheet as String,
+            kUTTypePresentation as String,
+            kUTTypeDatabase as String
+        ]
+        let documentPicker = UIDocumentPickerViewController(documentTypes: types, in: .import)
+        
+            documentPicker.allowsMultipleSelection = false
+        if #available(iOS 13.0, *) {
+            documentPicker.shouldShowFileExtensions = true
+        } else {
+            // Fallback on earlier versions
+        }
+        documentPicker.delegate = self
+        documentPicker.modalPresentationStyle = .fullScreen
+            present(documentPicker, animated: true, completion: nil)
+    }
+}
+
+extension UpdateTicketViewController:UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if (info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage) != nil {
+//            imageRetrieved.contentMode = .ScaleAspectFill
+//            imageRetrieved.image = pickedImage
+        }
+        var selectedImage: UIImage!
+        if let image = info[UIImagePickerController.InfoKey.editedImage.rawValue] as? UIImage {
+               selectedImage = image
+        } else if let image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+               selectedImage = image
+           }
+//           self.profileImageView.image = selectedImage
+
+        if (picker.sourceType == UIImagePickerController.SourceType.camera) {
+
+               let imgName = UUID().uuidString
+               let documentDirectory = NSTemporaryDirectory()
+               let localPath = documentDirectory.appending(imgName)
+
+            let data = selectedImage.jpegData(compressionQuality: 0.3)! as NSData
+               data.write(toFile: localPath, atomically: true)
+               let photoURL = URL.init(fileURLWithPath: localPath)
+print("Camera Url",photoURL)
+
+           }
+        if let imgUrl = info[UIImagePickerController.InfoKey.imageURL.rawValue] as? URL{
+                let imgName = imgUrl.lastPathComponent
+                let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+                let localPath = documentDirectory?.appending(imgName)
+
+            let image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as! UIImage
+                let data = image.pngData()! as NSData
+                data.write(toFile: localPath!, atomically: true)
+                //let imageData = NSData(contentsOfFile: localPath!)!
+                let photoURL = URL.init(fileURLWithPath: localPath!)//NSURL(fileURLWithPath: localPath!)
+                print("Gallery Url",photoURL)
+            }
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UpdateTicketViewController: UIDocumentPickerDelegate{
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+       guard let myURL = urls.first else {
+           return
+       }
+       print("import result : \(myURL)")
+       dismiss(animated: true, completion: nil)
+   }
+        
+   
+
+    func documentMenu(_ documentMenu:UIDocumentMenuViewController, didPickDocumentPicker documentPicker: UIDocumentPickerViewController) {
+       documentPicker.delegate = self
+       present(documentPicker, animated: true, completion: nil)
+   }
+
+
+   func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+       print("view was cancelled")
+       dismiss(animated: true, completion: nil)
+   }
+   
 }
