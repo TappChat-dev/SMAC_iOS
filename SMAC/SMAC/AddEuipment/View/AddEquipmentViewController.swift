a3baae185574f1dc7bf8a0d0e5dff6e9d00fcfb6
TappChat-dev <tappchat.dev@gmail.com>
2021-09-02 14:08:45 +0530

    add controller for add equipment

---
+++ SMAC/SMAC/AddEuipment/View/AddEquipmentViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddEquipmentViewController.swift
+//  SMAC
+//
+//  Created by MAC on 02/09/21.
+//
+
+import UIKit
+
+class AddEquipmentViewController: UIViewController {
+    @IBOutlet weak var equipmentNameTxt: UITextField!
+    @IBOutlet weak var equipmentTypeTxt: UITextField!
+    @IBOutlet weak var modelNoTxt: UITextField!
+    @IBOutlet weak var equipmentNoTxt: UITextField!
+    @IBOutlet weak var btnReset:UIButton!
+    @IBOutlet weak var btnSave:UIButton!
+    @IBOutlet weak var btnBack:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
