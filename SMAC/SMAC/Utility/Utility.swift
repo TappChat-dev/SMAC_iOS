800271c06c78e8b7a4ff29ea7cae3f29ac546aaf
TappChat-dev <tappchat.dev@gmail.com>
2021-09-07 11:09:05 +0530

    add status bar code

--- SMAC/SMAC/Utility/Utility.swift
+++ SMAC/SMAC/Utility/Utility.swift
@@ -149,3 +149,35 @@ extension UIColor{
          )
      }
 }
+
+
+extension UIApplication {
+
+    var statusBarUIView: UIView? {
+
+        if #available(iOS 13.0, *) {
+            let tag = 3848245
+
+            let keyWindow: UIWindow? = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+
+            if let statusBar = keyWindow?.viewWithTag(tag) {
+                return statusBar
+            } else {
+                let height = keyWindow?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
+                let statusBarView = UIView(frame: height)
+                statusBarView.tag = tag
+                statusBarView.layer.zPosition = 999999
+
+                keyWindow?.addSubview(statusBarView)
+                return statusBarView
+            }
+
+        } else {
+
+            if responds(to: Selector(("statusBar"))) {
+                return value(forKey: "statusBar") as? UIView
+            }
+        }
+        return nil
+      }
+}
